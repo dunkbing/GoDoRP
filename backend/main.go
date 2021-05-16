15e55aa5910cba3acf7605475ff65eed9bf03151
binh.buidang <dangbinh4869@gmail.com>
2021-05-17 01:26:38 +0700

    initial setup

---
+++ backend/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	l := log.New(os.Stdout, "backend", log.LstdFlags)
+	sm := mux.NewRouter()
+
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  time.Second,
+		WriteTimeout: time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, syscall.SIGTERM)
+
+	sig := <-sigChan
+	l.Println("Receive terminate, shutting down", sig)
+
+	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	s.Shutdown(tc)
+}
