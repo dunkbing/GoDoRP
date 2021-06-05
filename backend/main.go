d32bac48422614b9a98d095446fc9861477cf7a9
binh.buidang <dangbinh4869@gmail.com>
2021-06-05 09:06:39 +0700

    [ADD] cors

--- backend/main.go
+++ backend/main.go
@@ -4,12 +4,18 @@ import (
 	"github.com/dunkbing/sfw-checker-viet/backend/database"
 	"github.com/dunkbing/sfw-checker-viet/backend/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	database.Connect()
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+		AllowOrigins:     "http://localhost:3000",
+	}))
 
 	routes.Setup(app)
 
