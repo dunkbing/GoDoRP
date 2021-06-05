9467ed45c34a6bcdb31311173aef0180da31998a
binh.buidang <dangbinh4869@gmail.com>
2021-06-05 16:39:45 +0700

    [UPDATE] login, logout with useeffect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
@@ -6,12 +8,27 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get('user');
+        setUser(response.data);
+      } catch (error) {
+        setUser(null);
+      }
+    })();
+  }, [loggedIn]);
   return (
     <div className="App">
       <BrowserRouter>
-        <Nav />
-        <Route path="/" exact component={Home} />
-        <Route path="/login" component={Login} />
+        <Nav user={user} setLoggedIn={() => setLoggedIn(false)} />
+        <Route path="/" exact component={() => <Home user={user} />} />
+        <Route
+          path="/login"
+          component={() => <Login setLoggedIn={() => setLoggedIn(true)} />}
+        />
         <Route path="/register" component={Register} />
       </BrowserRouter>
     </div>
--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,28 +1,52 @@
-import React from 'react';
+import axios from 'axios';
+import { FC } from 'react';
 import { Link } from 'react-router-dom';
 
-const Nav = () => {
-  return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-      <ul className="navbar-nav mr-auto">
+const Nav: FC<{ user: any; setLoggedIn: () => void }> = ({
+  user,
+  setLoggedIn,
+}) => {
+  let logoutLink;
+  if (user) {
+    const logout = async (e: React.MouseEvent<HTMLAnchorElement>) => {
+      await axios.post('logout');
+      setLoggedIn();
+    };
+    logoutLink = (
+      <ul className="navbar-nav my-2 my-lg-0">
         <li className="nav-item">
-          <Link to="/" href="#" className="nav-link">
-            Home
+          <Link onClick={logout} to="login" className="nav-link">
+            Logout
           </Link>
         </li>
       </ul>
-      <ul className="navbar-nav my-2 my-lg-8">
+    );
+  } else {
+    logoutLink = (
+      <ul className="navbar-nav my-2 my-lg-0">
         <li className="nav-item">
-          <Link to="login" href="#" className="nav-link">
+          <Link to="login" className="nav-link">
             Login
           </Link>
         </li>
         <li className="nav-item">
-          <Link to="register" href="#" className="nav-link">
+          <Link to="register" className="nav-link">
             Register
           </Link>
         </li>
       </ul>
+    );
+  }
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
+      </ul>
+      {logoutLink}
     </nav>
   );
 };
