8eafe80e21de80f7d0b202ea3f3b1e28a1996448
binh.buidang <dangbinh4869@gmail.com>
2021-06-05 09:35:08 +0700

    [ADD] logout route

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -8,5 +8,6 @@ import (
 func Setup(app *fiber.App) {
 	app.Post("/register", controllers.Register)
 	app.Post("login", controllers.Login)
+	app.Post("logout", controllers.Logout)
 	app.Get("user", controllers.User)
 }
