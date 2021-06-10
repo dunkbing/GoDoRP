65f093dc25f9fcb7d21663fe0ebec8e7f036cb47
binh.buidang <dangbinh4869@gmail.com>
2021-06-10 10:28:06 +0700

    [ADD] register and login user

--- backend/models/user.go
+++ backend/models/user.go
@@ -2,8 +2,21 @@ package models
 
 type User struct {
 	Id        uint   `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
 	Email     string `json:"email" gorm:"unique"`
 	Password  string `json:"-"`
+}
+
+type RegisterUser struct {
+	FirstName   string `json:"firstName"`
+	LastName    string `json:"lastName"`
+	Email       string `json:"email"`
+	Password    string `json:"password"`
+	ConfirmPass string `json:"confirmPass"`
+}
+
+type LoginUser struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
