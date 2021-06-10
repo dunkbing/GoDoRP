59d08ce032a33538ffefd676f272c7e2892326d9
binh.buidang <dangbinh4869@gmail.com>
2021-06-10 14:46:58 +0700

    [ADD] loadEnv

--- backend/database/connect.go
+++ backend/database/connect.go
@@ -6,12 +6,20 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+	"os"
 )
 
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "host=localhost user=postgres password=sa dbname=test port=5432 sslmode=disable"
+	var (
+		host     = os.Getenv("DB_HOST")
+		user     = os.Getenv("DB_USER")
+		password = os.Getenv("DB_PASSWORD")
+		dbPort   = os.Getenv("DB_PORT")
+		dbName   = os.Getenv("DB_NAME")
+	)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbName, dbPort)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
