96c2cf86ce9ecb07405198aa329f069ce1c686cf
binh.buidang <dangbinh4869@gmail.com>
2021-06-10 17:23:32 +0700

    [REFACTOR] add tags

--- backend/api/app_error.go
+++ backend/api/app_error.go
@@ -1,13 +1,13 @@
 package api
 
 type HttpError struct {
-	Id            string `json:"id,omitempty"`
-	Message       string `json:"message"`                  // Message to be display to the end user without debugging information
-	DetailedError string `json:"detailed_error,omitempty"` // Internal error string to help the developer
-	RequestId     string `json:"request_id,omitempty"`     // The RequestId that's also set in the header
-	StatusCode    int    `json:"status_code,omitempty"`    // The http status code
-	Where         string `json:"-"`                        // The function where it happened in the form of Struct.Func
-	IsOAuth       bool   `json:"is_oauth,omitempty"`       // Whether the error is OAuth specific
+	Id            string `json:"-"`
+	Message       string `json:"message"`                 // Message to be display to the end user without debugging information
+	DetailedError string `json:"detailedError,omitempty"` // Internal error string to help the developer
+	RequestId     string `json:"-"`                       // The RequestId that's also set in the header
+	StatusCode    int    `json:"statusCode,omitempty"`    // The http status code
+	Where         string `json:"-"`                       // The function where it happened in the form of Struct.Func
+	IsOAuth       bool   `json:"-"`                       // Whether the error is OAuth specific
 	// params        map[string]interface{}
 }
 
--- backend/models/user.go
+++ backend/models/user.go
@@ -1,22 +1,26 @@
 package models
 
 type User struct {
-	Id        uint   `json:"id"`
+	Id        uint   `json:"id" swaggerignore:"true"`
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
 	Email     string `json:"email" gorm:"unique"`
-	Password  string `json:"-"`
+	Password  string `json:"-" swaggerignore:"true"`
 }
 
 type RegisterUser struct {
-	FirstName   string `json:"firstName"`
-	LastName    string `json:"lastName"`
-	Email       string `json:"email"`
-	Password    string `json:"password"`
-	ConfirmPass string `json:"confirmPass"`
+	FirstName   string `json:"firstName" example:"Bing"`
+	LastName    string `json:"lastName" example:"Bui"`
+	Email       string `json:"email" example:"bing@dep.chai"`
+	Password    string `json:"password" example:"Bingvip69."`
+	ConfirmPass string `json:"confirmPass" example:"Bingvip69."`
 }
 
 type LoginUser struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" example:"bing@dep.chai"`
+	Password string `json:"password" example:"Bingvip69."`
+}
+
+type LoginResponse struct {
+	Jwt string `json:"jwt"`
 }
