caf9acbc96da94e323c389461b06bc05c9c1dec1
binh.buidang <dangbinh4869@gmail.com>
2021-06-10 14:46:31 +0700

    [ADD] swagger and loadEnv

--- backend/main.go
+++ backend/main.go
@@ -5,9 +5,28 @@ import (
 	"github.com/dunkbing/sfw-checker-viet/backend/database"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+	"os"
 )
 
+// @title Learning go
+// @version 2.0
+// @description This is a sample server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath
+// @schemes http
 func main() {
+	_ = godotenv.Load()
+
 	database.Connect()
 
 	defer database.Close()
@@ -16,7 +35,7 @@ func main() {
 
 	app.Use(cors.New(cors.Config{
 		AllowCredentials: true,
-		AllowOrigins:     "http://localhost:3000",
+		AllowOrigins:     os.Getenv("ALLOW_ORIGIN"),
 	}))
 
 	api.Init(app)
